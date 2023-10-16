837142fe0dae20952313b6e314b6a30e99ae6a38
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-10-16 18:57:46 +0000

    type casting in js

---
+++ js/Typecasting.js
@@ -0,0 +1,66 @@
+// let a=12;
+// console.log(typeof a);
+// console.log(typeof(a));
+// let x=String(a);
+// console.log(typeof x);
+// console.log(x);
+
+// 2nd
+/*
+let a="12";
+console.log(typeof a);
+console.log(typeof(a));
+let x=Number(a);
+console.log(typeof x);
+console.log(x);
+*/
+// 3rd
+/*
+let a=null;
+console.log(typeof a);
+console.log(typeof(a));
+let x=String(a);
+console.log(typeof x);
+console.log(x);
+*/
+//4th
+// let a=undefined;
+// console.log(typeof a);
+// console.log(typeof(a));
+// let x=Number(a);
+// console.log(typeof x);
+// console.log(x);
+
+/*
+let a=undefined;
+console.log(typeof a);
+console.log(typeof(a));
+let x=String(a);
+console.log(typeof x);
+console.log(x);
+*/
+// vvi point
+/*
+let a="shivkant";
+console.log(typeof a);
+console.log(typeof(a));
+let x=Number(a);
+console.log(typeof x);
+console.log(x);
+*/
+let a=true;
+console.log(typeof a);
+console.log(typeof(a));
+let x=Number(a);
+console.log(typeof x);
+console.log(x);
+
+// notes
+
+// "12"==> 12
+// "12abc"==>NaN
+// true=1 ;false=0;
+// string -->Number==>NaN;
+// ""=false
+//1=true,0=false
+//"sk"=true
