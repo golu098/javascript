484c0b43e41fe4341ed73b2ec86f103ee89d227e
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-10-17 16:06:38 +0000

    data type

---
+++ basic_js/DataType.js
@@ -0,0 +1,12 @@
+"use strict";// treat all js code as newer version
+// code readability should be high
+// ECMA script :-offical docx; ek standards hai like ASCII
+ // number
+ // string 
+ // bigint
+ // boolean
+ // null:- standalone val; vvi:- object
+ //undefined:- data type hai when we do typeof then it shows
+ // symbol
+// object
+// 
