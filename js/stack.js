d96c4b4192de229210e3f456e757782d56abc5fd
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-10-17 17:58:03 +0000

    stack

--- js/stack.js
+++
@@ -1,8 +0,0 @@
-// stack(primitive)
-// heap(reference or non -primitive)
-let name="shiv kant"
-let anotherName=name
-// it will only overwrite the copied another name which is in stack
-anotherName="golu"
-console.log(name);
-console.log(anotherName);
