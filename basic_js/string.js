c3947cea5c3ce5c063381c06b4e739884905b527
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-10-17 16:07:03 +0000

    string

---
+++ basic_js/string.js
@@ -0,0 +1,16 @@
+const name="shiv kant "
+const age="20"
+
+// console.log(`Hello my name is ${name} and my age is ${age}`);
+const myName=new String('shiv kant');
+// console.log(myName.length);
+
+console.log(myName.toUpperCase());
+
+const newname=myName.substring(0,6);
+console.log(newname);
+// Note:-
+// ``:- string interpolation 
+// slice:-negative value dete hai to wo reverse se start krta hai
+// trim:- start and endspace remove
+// spilt:- string ko array m bna dete hai aur using specific things to sort it like on the basis of -,*
