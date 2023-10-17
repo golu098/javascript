3f5bf9c0ed268ecb93b49d3c1efae8a7fee88b1c
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-10-17 16:06:47 +0000

    heap

---
+++ basic_js/heap.js
@@ -0,0 +1,10 @@
+let obj1={
+    name:"shiv kant",
+    msg:"kaise ho app!"
+}
+let obj2=obj1;
+obj2.name="golu"
+console.log(obj1.name);
+console.log(obj2.name);
+
+// it happend bcz in heap we pass refrence not copy 
