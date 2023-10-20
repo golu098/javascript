a2e8fface55588ebbff9c6f16d4801d447cd1a12
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-10-20 15:20:46 +0000

    arrow

---
+++ function/this.js
@@ -0,0 +1,6 @@
+// use to refer  current context(about whome we are talking )
+// browser ke andr sabsa zyda object :- window
+//iifi:- global scope ke pollution se dikkt hota hai islea iifi ka use krete hai   
+// synatx:- () ()
+    //    function def ,execution call both ()
+    
